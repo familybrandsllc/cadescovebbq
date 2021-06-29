41dc6d1241e9e4055d8f45ee8efc50cae75fb6ef
familybrandsllc <86477587+familybrandsllc@users.noreply.github.com>
2021-06-29 14:20:28 -0400

    Create Dinner_Delight-GeoJSON.js

---
+++ geoJSON/Dinner_Delight-GeoJSON.js
@@ -0,0 +1,832 @@
+eqfeed_callback({
+   "type": "FeatureCollection",
+   "features": [
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.617232,36.305124 ]
+    },
+    "properties": {
+    "Name":"Cash Saver #11",
+    "Street":"213 West Main",
+    "City":"Hendersonville",
+    "State":"TN",
+    "Zip":37075
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -87.058718,36.266855 ]
+    },
+    "properties": {
+    "Name":"Cash Saver #75",
+    "Street":"795 South Main Street",
+    "City":"Ashland City",
+    "State":"TN",
+    "Zip":37015
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.882456,36.507177 ]
+    },
+    "properties": {
+    "Name":"Hill's #54",
+    "Street":"519 Memorial Blvd.",
+    "City":"Springfield",
+    "State":"TN",
+    "Zip":37172
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.08154,35.827303 ]
+    },
+    "properties": {
+    "Name":"Piggly Wiggly #44",
+    "Street":"834 West Main Street",
+    "City":"Woodbury",
+    "State":"TN",
+    "Zip":37190
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.759044,36.229947 ]
+    },
+    "properties": {
+    "Name":"H.G. Hill #9",
+    "Street":"3105 Dickerson Road",
+    "City":"Nashville",
+    "State":"TN",
+    "Zip":37207
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.775747,36.34145 ]
+    },
+    "properties": {
+    "Name":"H.G. Hill #38",
+    "Street":"212 Goodlettsville Plaza",
+    "City":"Goodlettsville",
+    "State":"TN",
+    "Zip":37072
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -87.346836,36.170708 ]
+    },
+    "properties": {
+    "Name":"Murphy's Cee Bee",
+    "Street":"3426 Hwy 48 North",
+    "City":"Charlotte",
+    "State":"TN",
+    "Zip":37036
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.157832,36.397114 ]
+    },
+    "properties": {
+    "Name":"Piggly Wiggly #11",
+    "Street":"130 McMurray Blvd.",
+    "City":"Hartsville",
+    "State":"TN",
+    "Zip":37074
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.285536,36.211559 ]
+    },
+    "properties": {
+    "Name":"Cash Saver #16",
+    "Street":"312 East High Street",
+    "City":"Lebanon",
+    "State":"TN",
+    "Zip":37087
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.767668,36.207593 ]
+    },
+    "properties": {
+    "Name":"Piggly Wiggly #34",
+    "Street":"2317 Dickerson Road",
+    "City":"Nashville",
+    "State":"TN",
+    "Zip":37207
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -87.304502,35.952204 ]
+    },
+    "properties": {
+    "Name":"Lyles Valu-Plus Foods",
+    "Street":"9592 Hwy 46 ",
+    "City":"Bon Aqua",
+    "State":"TN",
+    "Zip":37025
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -84.525166,35.324057 ]
+    },
+    "properties": {
+    "Name":"Piggly Wiggly #2",
+    "Street":"740 N. Tennessee Avenue",
+    "City":"Etowah",
+    "State":"TN",
+    "Zip":37331
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -87.200224,35.545512 ]
+    },
+    "properties": {
+    "Name":"Tietgens Mt. Pleasant CB",
+    "Street":"815 N Main Street",
+    "City":"Mt. Pleasant",
+    "State":"TN",
+    "Zip":38474
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -87.326596,35.248438 ]
+    },
+    "properties": {
+    "Name":"Lawrenceburg Cash Saver",
+    "Street":"818 N Locust Ave",
+    "City":"Lawrenceburg",
+    "State":"TN",
+    "Zip":38464
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -87.280308,35.430304 ]
+    },
+    "properties": {
+    "Name":"Super Rama #7",
+    "Street":"4955 Highway 43",
+    "City":"Mt. Pleasant",
+    "State":"TN",
+    "Zip":38474
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -87.434947,36.640151 ]
+    },
+    "properties": {
+    "Name":"Oak Grove Piggly Wiggly",
+    "Street":"3441 Fort Campbell Blvd.",
+    "City":"Clarksville",
+    "State":"TN",
+    "Zip":37040
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.450561,36.389046 ]
+    },
+    "properties": {
+    "Name":"HG Hills Market",
+    "Street":"200 West Broadway Street",
+    "City":"Gallatin",
+    "State":"TN",
+    "Zip":37066
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.574746,34.099796 ]
+    },
+    "properties": {
+    "Name":"Lucky's #94",
+    "Street":"70670 Hwy 231 ",
+    "City":"Blountsville",
+    "State":"AL",
+    "Zip":35031
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -85.331706,36.380841 ]
+    },
+    "properties": {
+    "Name":"Jerry's IGA",
+    "Street":"800 Overton Plaza",
+    "City":"Livingston",
+    "State":"TN",
+    "Zip":38570
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.832463,36.193225 ]
+    },
+    "properties": {
+    "Name":"Bordeaux Foods",
+    "Street":"3205 Clarksville Hwy",
+    "City":"Nashville",
+    "State":"TN",
+    "Zip":37218
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -87.034917,35.611408 ]
+    },
+    "properties": {
+    "Name":"Columbia Supermarket",
+    "Street":"1005 Carmack Blvd",
+    "City":"Columbia",
+    "State":"TN",
+    "Zip":38401
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.653529,36.256124 ]
+    },
+    "properties": {
+    "Name":"Piggly Wiggly",
+    "Street":"1439 Robinson Road",
+    "City":"Old Hickory",
+    "State":"TN",
+    "Zip":37138
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -87.123272,35.981534 ]
+    },
+    "properties": {
+    "Name":"Food Saver",
+    "Street":"2080 Fairview Blvd.",
+    "City":"Fairview",
+    "State":"TN",
+    "Zip":37062
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.740247,36.101528 ]
+    },
+    "properties": {
+    "Name":"Woodbine Fresh Market",
+    "Street":"3302 Nolensville Road",
+    "City":"Nashville",
+    "State":"TN",
+    "Zip":37211
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -87.389641,36.468133 ]
+    },
+    "properties": {
+    "Name":"Hilltop Cee Bee #2",
+    "Street":"400 Hwy 149",
+    "City":"Clarksville",
+    "State":"TN",
+    "Zip":37040
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -84.790827,35.520068 ]
+    },
+    "properties": {
+    "Name":"Piggly Wiggly #3",
+    "Street":"17619 State Hwy 58",
+    "City":"Decatur",
+    "State":"TN",
+    "Zip":37322
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -87.43105,35.085359 ]
+    },
+    "properties": {
+    "Name":"Tietgens Loretto Cash Saver",
+    "Street":"405 N. Military",
+    "City":"Loretto",
+    "State":"TN",
+    "Zip":38469
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -84.958666,35.303249 ]
+    },
+    "properties": {
+    "Name":"Fresh N Low Cost Plus #16",
+    "Street":"101 SR Hwy 58 So.",
+    "City":"Georgetown",
+    "State":"TN",
+    "Zip":37336
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.20128,34.277954 ]
+    },
+    "properties": {
+    "Name":"Piggly Wiggly #500",
+    "Street":"250 Alabama Hwy 75, North",
+    "City":"Albertville",
+    "State":"AL",
+    "Zip":35951
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.315586,34.352798 ]
+    },
+    "properties": {
+    "Name":"Piggly Wiggly #502",
+    "Street":"1455 Sunset Drive",
+    "City":"Guntersville",
+    "State":"AL",
+    "Zip":35976
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.851379,34.99401 ]
+    },
+    "properties": {
+    "Name":"Piggly Wiggly #80",
+    "Street":"26257 Main Street",
+    "City":"Ardmore",
+    "State":"TN",
+    "Zip":38449
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -85.449748,35.92477 ]
+    },
+    "properties": {
+    "Name":"Floyd's Cash Saver Foods",
+    "Street":"542 Bockman Way",
+    "City":"Sparta",
+    "State":"TN",
+    "Zip":38583
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -88.276358,36.320007 ]
+    },
+    "properties": {
+    "Name":"Lakeway IGA",
+    "Street":"2601 East Wood Street",
+    "City":"Paris",
+    "State":"TN",
+    "Zip":38242
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -88.638815,36.729874 ]
+    },
+    "properties": {
+    "Name":"Food Giant #245",
+    "Street":"300 West Farthing",
+    "City":"Mayfield",
+    "State":"KY",
+    "Zip":42066
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.252744,34.527842 ]
+    },
+    "properties": {
+    "Name":"Grant Foodland",
+    "Street":"20 Main St",
+    "City":"Grant",
+    "State":"AL",
+    "Zip":35747
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -87.6678,34.73373 ]
+    },
+    "properties": {
+    "Name":"Foodland",
+    "Street":"3000 Woodward Ave",
+    "City":"Muscle Shoals",
+    "State":"AL",
+    "Zip":35661
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -87.275193,34.483092 ]
+    },
+    "properties": {
+    "Name":"Gateway Foodland",
+    "Street":"15342 Court St",
+    "City":"Moulton",
+    "State":"AL",
+    "Zip":35650
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.699039,35.616225 ]
+    },
+    "properties": {
+    "Name":"Brooks Grocery",
+    "Street":"4666 Nashville Hwy",
+    "City":"Chapel Hill",
+    "State":"TN",
+    "Zip":37034
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.025342,36.072417 ]
+    },
+    "properties": {
+    "Name":"Prichards Foods",
+    "Street":"855 Brush Creek Hwy",
+    "City":"Alexandria",
+    "State":"TN",
+    "Zip":37012
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.387843,35.845962 ]
+    },
+    "properties": {
+    "Name":"Juniors Foodland",
+    "Street":"323 E Main St",
+    "City":"Murfreesboro",
+    "State":"TN",
+    "Zip":37130
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.571935,35.152544 ]
+    },
+    "properties": {
+    "Name":"Geralds Foodland",
+    "Street":"200 W College St",
+    "City":"Fayetteville",
+    "State":"TN",
+    "Zip":37334
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.161567,36.399273 ]
+    },
+    "properties": {
+    "Name":"Hartsville Foodland",
+    "Street":"106 McMurry Blvd E.",
+    "City":"Hartsville",
+    "State":"TN",
+    "Zip":37074
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -85.962932,36.267684 ]
+    },
+    "properties": {
+    "Name":"Save-A-Way",
+    "Street":"80 Dixon Springs Hwy",
+    "City":"Carthage",
+    "State":"TN",
+    "Zip":37030
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.87477,36.280117 ]
+    },
+    "properties": {
+    "Name":"Tony's Foodland",
+    "Street":"5529 Clarksville Hwy",
+    "City":"Joelton",
+    "State":"TN",
+    "Zip":37080
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.83943,36.208561 ]
+    },
+    "properties": {
+    "Name":"Hunter's Hometown Foods",
+    "Street":"3910 Clarksville Hwy",
+    "City":"Nashville",
+    "State":"TN",
+    "Zip":37218
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.77076,36.397011 ]
+    },
+    "properties": {
+    "Name":"Holts Hometown Foods",
+    "Street":"1778 Hwy 41 S",
+    "City":"Ridgetop",
+    "State":"TN",
+    "Zip":37152
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -87.625721,36.104852 ]
+    },
+    "properties": {
+    "Name":"Chappell's Hometown Foods",
+    "Street":"10041 Hwy 70 E",
+    "City":"McEwen",
+    "State":"TN",
+    "Zip":37101
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -85.285553,34.704288 ]
+    },
+    "properties": {
+    "Name":"Chappell's",
+    "Street":"321 W Patton Ave",
+    "City":"Lafayette",
+    "State":"GA",
+    "Zip":30728
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.999712,34.589057 ]
+    },
+    "properties": {
+    "Name":"Parker's Food Valu",
+    "Street":"1209-D Danville Rd",
+    "City":"Decatur",
+    "State":"AL",
+    "Zip":35601
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.472451,35.496804 ]
+    },
+    "properties": {
+    "Name":"Shelbyville Piggly Wiggly",
+    "Street":"874 Union St",
+    "City":"Shelbyville",
+    "State":"TN",
+    "Zip":37160
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.504154,34.357108 ]
+    },
+    "properties": {
+    "Name":"Warehouse Grocery #3040",
+    "Street":"2250 N Brindlee Mtn Pky",
+    "City":"Arab",
+    "State":"AL",
+    "Zip":35016
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.838431,34.159616 ]
+    },
+    "properties": {
+    "Name":"Warehouse Grocery #3070",
+    "Street":"1615 Town Square",
+    "City":"Cullman",
+    "State":"AL",
+    "Zip":35055
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -87.077066,35.599806 ]
+    },
+    "properties": {
+    "Name":"Piggly Wiggly",
+    "Street":"1412 Trotwood Ave",
+    "City":"Columbia",
+    "State":"TN",
+    "Zip":38401
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.040469,32.64067 ]
+    },
+    "properties": {
+    "Name":"Piggly Wiggly ",
+    "Street":"515 Kowaliga Rd",
+    "City":"Eclectic",
+    "State":"AL",
+    "Zip":36024
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -87.891269,36.480419 ]
+    },
+    "properties": {
+    "Name":"Piggly Wiggly",
+    "Street":"1536 Donelson Pkwy",
+    "City":"Dover",
+    "State":"TN",
+    "Zip":37058
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -87.713004,36.316601 ]
+    },
+    "properties": {
+    "Name":"Piggly Wiggly",
+    "Street":"3611 West Main",
+    "City":"Erin ",
+    "State":"TN",
+    "Zip":37061
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.309414,32.350408 ]
+    },
+    "properties": {
+    "Name":"Piggly Wiggly",
+    "Street":"15 W Fairview Ave",
+    "City":"Montgomery",
+    "State":"AL",
+    "Zip":36105
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -87.975808,34.755637 ]
+    },
+    "properties": {
+    "Name":"Piggly Wiggly",
+    "Street":"8075 Hwy 72",
+    "City":"Cherokee",
+    "State":"AL",
+    "Zip":35616
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -83.956548,35.904038 ]
+    },
+    "properties": {
+    "Name":"Food City",
+    "Street":"6710 Malone Creek Drive",
+    "City":"Knoxville",
+    "State":"TN",
+    "Zip":37931
+    }
+  }
+]
+}
+);
