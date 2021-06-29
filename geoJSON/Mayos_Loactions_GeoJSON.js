5bc9060d273da5ffd2c1743c016e9371219816f3
familybrandsllc <86477587+familybrandsllc@users.noreply.github.com>
2021-06-29 14:24:34 -0400

    Create Mayos_Loactions_GeoJSON.js

---
+++ geoJSON/Mayos_Loactions_GeoJSON.js
@@ -0,0 +1,384 @@
+eqfeed_callback({
+   "type": "FeatureCollection",
+   "features": [
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.617232,36.305124 ]
+    },
+    "properties": {
+    "Name":"Cash Saver #11",
+    "Street":"213 West Main",
+    "City":"Hendersonville",
+    "State":"TN",
+    "Zip":37075
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -87.034976,36.39498 ]
+    },
+    "properties": {
+    "Name":"H.G. Hill #86",
+    "Street":"2498 Hwy 49 East",
+    "City":"Pleasant View",
+    "State":"TN",
+    "Zip":37146
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.882456,36.507177 ]
+    },
+    "properties": {
+    "Name":"Hill's #54",
+    "Street":"519 Memorial Blvd.",
+    "City":"Springfield",
+    "State":"TN",
+    "Zip":37172
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.813585,36.144552 ]
+    },
+    "properties": {
+    "Name":"Piggly Wiggly #41",
+    "Street":"2900 West End Avenue",
+    "City":"Nashville",
+    "State":"TN",
+    "Zip":37203
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.759044,36.229947 ]
+    },
+    "properties": {
+    "Name":"H.G. Hill #9",
+    "Street":"3105 Dickerson Road",
+    "City":"Nashville",
+    "State":"TN",
+    "Zip":37207
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.775747,36.34145 ]
+    },
+    "properties": {
+    "Name":"H.G. Hill #38",
+    "Street":"212 Goodlettsville Plaza",
+    "City":"Goodlettsville",
+    "State":"TN",
+    "Zip":37072
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.796202,36.118482 ]
+    },
+    "properties": {
+    "Name":"Osborne's Bi-Rite #14",
+    "Street":"3116 Belmont Blvd.",
+    "City":"Nashville",
+    "State":"TN",
+    "Zip":37212
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -87.346836,36.170708 ]
+    },
+    "properties": {
+    "Name":"Murphy's Cee Bee",
+    "Street":"3426 Hwy 48 North ",
+    "City":"Charlotte",
+    "State":"TN",
+    "Zip":37036
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -87.123272,35.981534 ]
+    },
+    "properties": {
+    "Name":"Food Saver",
+    "Street":"2080 Fairview Blvd.",
+    "City":"Fairview",
+    "State":"TN",
+    "Zip":37062
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -87.389641,36.468133 ]
+    },
+    "properties": {
+    "Name":"Hilltop Cee Bee #2",
+    "Street":"400 Hwy 149",
+    "City":"Clarksville",
+    "State":"TN",
+    "Zip":37040
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.767668,36.207593 ]
+    },
+    "properties": {
+    "Name":"Piggly Wiggly #34",
+    "Street":"2317 Dickerson Road",
+    "City":"Nashville",
+    "State":"TN",
+    "Zip":37207
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -87.304502,35.952204 ]
+    },
+    "properties": {
+    "Name":"Lyles Valu-Plus Foods",
+    "Street":"9592 Hwy 46 ",
+    "City":"Bon Aqua",
+    "State":"TN",
+    "Zip":37025
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.653529,36.256124 ]
+    },
+    "properties": {
+    "Name":"Piggly Wiggly",
+    "Street":"1439 Robinson Road",
+    "City":"Old Hickory",
+    "State":"TN",
+    "Zip":37138
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -87.326596,35.248438 ]
+    },
+    "properties": {
+    "Name":"Lawrenceburg Cash Saver",
+    "Street":"818 N Locust Ave",
+    "City":"Lawrenceburg",
+    "State":"TN",
+    "Zip":38464
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -87.434947,36.640151 ]
+    },
+    "properties": {
+    "Name":"Oak Grove Piggly Wiggly",
+    "Street":"3441 Fort Campbell Blvd.",
+    "City":"Clarksville",
+    "State":"TN",
+    "Zip":37040
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -85.931726,36.986791 ]
+    },
+    "properties": {
+    "Name":"Houchens Market #1",
+    "Street":"619 South L. Rogers Blvd",
+    "City":"Glasgow",
+    "State":"KY",
+    "Zip":42141
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -82.003033,39.946286 ]
+    },
+    "properties": {
+    "Name":"Riesbeck's #8",
+    "Street":"800 Howard Street",
+    "City":"Zanesville",
+    "State":"OH",
+    "Zip":43701
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.395877,37.010338 ]
+    },
+    "properties": {
+    "Name":"Price Less IGA #484",
+    "Street":"3170 Louisville Road",
+    "City":"Bowling Green",
+    "State":"KY",
+    "Zip":42101
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -87.484993,36.871563 ]
+    },
+    "properties": {
+    "Name":"Piggly Wiggly #70",
+    "Street":"128 North Main",
+    "City":"Hopkinsville",
+    "State":"KY",
+    "Zip":42240
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -87.15513,36.809556 ]
+    },
+    "properties": {
+    "Name":"Food Giant #78",
+    "Street":"202 West Main",
+    "City":"Elkton",
+    "State":"KY",
+    "Zip":42220
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -87.510167,37.304335 ]
+    },
+    "properties": {
+    "Name":"Market Place #83",
+    "Street":"1771 South Main",
+    "City":"Madisonville",
+    "State":"KY",
+    "Zip":42431
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.291079,36.224876 ]
+    },
+    "properties": {
+    "Name":"Al's Foodland",
+    "Street":"1006 N Cumberland ",
+    "City":"Lebanon",
+    "State":"TN",
+    "Zip":37087
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.83943,36.208561 ]
+    },
+    "properties": {
+    "Name":"Hunter's Hometown Foods",
+    "Street":"3910 Clarksville Hwy",
+    "City":"Nashville",
+    "State":"TN",
+    "Zip":37218
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -87.35148,36.032771 ]
+    },
+    "properties": {
+    "Name":"Chappell's CB",
+    "Street":"990 Pomona Rd",
+    "City":"Dickson",
+    "State":"TN",
+    "Zip":37055
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -87.363615,36.076176 ]
+    },
+    "properties": {
+    "Name":"Chappell's CB",
+    "Street":"691 Hwy 70 E",
+    "City":"Dickson",
+    "State":"TN",
+    "Zip":37055
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -87.227261,36.108936 ]
+    },
+    "properties": {
+    "Name":"Chappell's CB",
+    "Street":"2005 Hwy 47",
+    "City":"White Bluff",
+    "State":"TN",
+    "Zip":37187
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -86.576359,34.738623 ]
+    },
+    "properties": {
+    "Name":"Star Super Market",
+    "Street":"702 Pratt Ave",
+    "City":"Huntsville",
+    "State":"AL",
+    "Zip":35801
+    }
+  }
+]
+}
+);
