bcc2f80aec2e9f9dbc32626813520fc11777e800
familybrandsllc <86477587+familybrandsllc@users.noreply.github.com>
2021-06-29 14:25:22 -0400

    Create Senor_Housers_Loactions_GeoJSON.js

---
+++ geoJSON/Senor_Housers_Loactions_GeoJSON.js
@@ -0,0 +1,426 @@
+eqfeed_callback({
+   "type": "FeatureCollection",
+   "features": [
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -83.956548,35.904038 ]
+    },
+    "properties": {
+    "Name":"Food City",
+    "Street":"6710 Malone Creek Drive",
+    "City":"Knoxville",
+    "State":"TN",
+    "Zip":37931
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -85.756521,38.104196 ]
+    },
+    "properties": {
+    "Name":"Cash Saver Cost Plus",
+    "Street":"501 Mt. Holly Road",
+    "City":"Fairdale",
+    "State":"KY",
+    "Zip":40118
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -84.525166,35.324057 ]
+    },
+    "properties": {
+    "Name":"Piggly Wiggly #2",
+    "Street":"740 N. Tennessee Avenue",
+    "City":"Etowah",
+    "State":"TN",
+    "Zip":37331
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -84.790827,35.520068 ]
+    },
+    "properties": {
+    "Name":"Piggly Wiggly #3",
+    "Street":"17619 State Hwy 58",
+    "City":"Decatur",
+    "State":"TN",
+    "Zip":37322
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -82.538283,37.387062 ]
+    },
+    "properties": {
+    "Name":"Quality Foods #3",
+    "Street":"5284 Collins Hwy.",
+    "City":"Robinson Creek",
+    "State":"KY",
+    "Zip":41560
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -82.406933,37.362923 ]
+    },
+    "properties": {
+    "Name":"Quality Foods #2",
+    "Street":"10023 Route 460",
+    "City":"Regina",
+    "State":"KY",
+    "Zip":41559
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -83.026187,36.759311 ]
+    },
+    "properties": {
+    "Name":"Grabeel's IGA",
+    "Street":"140 East Morgan Avenue",
+    "City":"Pennington Gap",
+    "State":"VA",
+    "Zip":24277
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -83.118599,36.686882 ]
+    },
+    "properties": {
+    "Name":"Grabeel's IGA",
+    "Street":"410 Hwy 58",
+    "City":"Jonesville",
+    "State":"VA",
+    "Zip":24263
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -85.021675,36.186702 ]
+    },
+    "properties": {
+    "Name":"Swafford & Sons IGA",
+    "Street":"6870 South York Hwy.",
+    "City":"Clarkrange",
+    "State":"TN",
+    "Zip":38553
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -85.71781,38.234798 ]
+    },
+    "properties": {
+    "Name":"Valu Market #47",
+    "Street":"1250 Bardstown Road",
+    "City":"Louisville",
+    "State":"KY",
+    "Zip":40204
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -82.560471,36.559826 ]
+    },
+    "properties": {
+    "Name":"Price Less IGA #469",
+    "Street":"4320A West Stone Drive",
+    "City":"Kingsport",
+    "State":"TN",
+    "Zip":37660
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -84.135569,36.366695 ]
+    },
+    "properties": {
+    "Name":"Lafollette IGA #55",
+    "Street":"1221 Jacksboro Pike",
+    "City":"Lafollette",
+    "State":"TN",
+    "Zip":37766
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -83.721689,36.037744 ]
+    },
+    "properties": {
+    "Name":"Price Less Foods #468",
+    "Street":"522 Andrew Johnson Hwy",
+    "City":"Strawberry Plains",
+    "State":"TN",
+    "Zip":37871
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -82.273844,37.17975 ]
+    },
+    "properties": {
+    "Name":"E & S Market",
+    "Street":"2222 Sandlick Road",
+    "City":"Birchleaf",
+    "State":"VA",
+    "Zip":24220
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -83.48203,36.119242 ]
+    },
+    "properties": {
+    "Name":"Price Less Foods #474",
+    "Street":"272 East Broadway Blvd.",
+    "City":"Jefferson City",
+    "State":"TN",
+    "Zip":37760
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -82.429759,36.12622 ]
+    },
+    "properties": {
+    "Name":"Price Less Foods #30",
+    "Street":"1609 Jackson Love Hwy",
+    "City":"Erwin",
+    "State":"TN",
+    "Zip":37650
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -82.507287,36.556102 ]
+    },
+    "properties": {
+    "Name":"Price Less Foods #478",
+    "Street":"3006 John B. Dennis Hwy",
+    "City":"Kingsport",
+    "State":"TN",
+    "Zip":37660
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -82.855032,37.13885 ]
+    },
+    "properties": {
+    "Name":"Priceless Foods #483",
+    "Street":"34 Whitesburg Plaza Road",
+    "City":"Whitesburg",
+    "State":"KY",
+    "Zip":41858
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -82.340181,36.304476 ]
+    },
+    "properties": {
+    "Name":"Price Less Foods #486",
+    "Street":"1950 South Roan Street",
+    "City":"Johnson City",
+    "State":"TN",
+    "Zip":37601
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -83.18275,36.862523 ]
+    },
+    "properties": {
+    "Name":"Harlan Cash Saver",
+    "Street":"416 South Main",
+    "City":"Harlan",
+    "State":"KY",
+    "Zip":40831
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -82.569986,36.607319 ]
+    },
+    "properties": {
+    "Name":"Price Less Foods #489",
+    "Street":"969 US Hwy 23 South",
+    "City":"Weber City",
+    "State":"VA",
+    "Zip":24290
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -82.547615,36.514384 ]
+    },
+    "properties": {
+    "Name":"Price Less Foods #487",
+    "Street":"1328 So. John B. Dennis Hwy",
+    "City":"Kingsport",
+    "State":"TN",
+    "Zip":37660
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -83.197032,35.966938 ]
+    },
+    "properties": {
+    "Name":"Price Less Foods #475",
+    "Street":"209 W. Broadway",
+    "City":"Newport",
+    "State":"TN",
+    "Zip":37821
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -85.031089,35.953957 ]
+    },
+    "properties": {
+    "Name":"Price Less IGA #495",
+    "Street":"75 Genesis Square",
+    "City":"Crossville",
+    "State":"TN",
+    "Zip":38555
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -83.314556,36.206801 ]
+    },
+    "properties": {
+    "Name":"Price Less Foods #473",
+    "Street":"1707 E. Andrew Johnson Pkwy",
+    "City":"Morristown",
+    "State":"TN",
+    "Zip":37814
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -84.616627,35.450503 ]
+    },
+    "properties": {
+    "Name":"Price Less IGA #43",
+    "Street":"1312 Decatur Pike",
+    "City":"Athens",
+    "State":"TN",
+    "Zip":37303
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -81.802256,36.465615 ]
+    },
+    "properties": {
+    "Name":"Price Less Foods #476",
+    "Street":"220 Pioneer Village",
+    "City":"Mountain City",
+    "State":"TN",
+    "Zip":37683
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -82.161696,36.625458 ]
+    },
+    "properties": {
+    "Name":"Price Less IGA #467",
+    "Street":"2012 Lee Highway",
+    "City":"Bristol",
+    "State":"VA",
+    "Zip":24202
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -82.470937,36.943287 ]
+    },
+    "properties": {
+    "Name":"Priceless Foods #477",
+    "Street":"109 Laurel Avenue",
+    "City":"Coeburn",
+    "State":"VA",
+    "Zip":24230
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -84.590242,36.103625 ]
+    },
+    "properties": {
+    "Name":"Houchens Market #41",
+    "Street":"1014 Main Street",
+    "City":"Wartburg",
+    "State":"TN",
+    "Zip":37887
+    }
+  }
+]
+}
+);
