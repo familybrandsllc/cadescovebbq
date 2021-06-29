5d963466e7ec890c2e2d745bb94fa33326249983
familybrandsllc <86477587+familybrandsllc@users.noreply.github.com>
2021-06-29 13:32:42 -0400

    Update index2.js

--- js/secondAttempt/index2.js
+++ js/secondAttempt/index2.js
@@ -11,7 +11,7 @@ function initMap() {
   // This example uses a local copy of the GeoJSON stored at
   // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
   script.src =
-    "https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js";
+    "https://cdn.jsdelivr.net/gh/familybrandsllc/cadescovebbq@main/geoJSON/geoJSONCades%20Cove%20GeoJson.js";
   document.getElementsByTagName("head")[0].appendChild(script);
 }
 
